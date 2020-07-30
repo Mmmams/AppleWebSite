3624f30b90ab4ea2fc00736e288a00fc3f38c5d1
Сергей Мамыкин <bymamykin@gmail.com>
2020-07-30 16:24:06 +0300

    кнопки назад в айфоне

--- index.js
+++ index.js
@@ -1,40 +1,3 @@
-// function buttonGetStart() {
-//   let name = document.getElementById("NamePlaceholder").value;
-//   let email = document.getElementById("emailPlaceholder").value;
-//   let HelloField = document.getElementsByClassName("formWrapper");
-//   HelloField[0].setAttribute(
-//     "style",
-//     "font-size: 26px; outline: none; margin: 5px;font-family: 'Notable', sans-serif; color: rgb(255, 255, 255); height: 7vh;"
-//   );
-//   HelloField[0].innerHTML = `Hello ${name}. Ready for more?`;
-// }
-// document
-//   .querySelector(".header-button")
-//   .addEventListener("click", buttonGetStart);
-
-// function productRender() {
-
-//   document.querySelector('mac').innerHTML = `
-//     <div class='buyForm'>
-// <div class='formInputs'>
-// <input type='text' placeholder='First name'  class='buyFormInput'/>
-// <input type='text' placeholder='Last name'  class='buyFormInput'/>
-// <input type='email' placeholder='email'  class='buyFormInput'/>
-// <select class='buyFormSelector'>
-//   <option>1</option>
-//   <option>2</option>
-//   <option>3</option>
-// </select>
-// </div>
-// <input type='submit'  class='buyFormInput'/>
-//     </div>`;
-// }
-// let arr = document.querySelectorAll(".macbtn");
-// console.log(arr);
-// arr.forEach((element) => {
-//   element.addEventListener("click", productRender);
-// });
-
 const bgImg = document.getElementsByClassName("header");
 let counter = 1;
 setInterval(() => {
@@ -53,17 +16,24 @@ document.getElementsByTagName("body")[0].addEventListener("click", clickFun);
 async function clickFun() {
   //  обработчик кнопки гетстарт
   if (event.target.classList.contains("header-button")) {
-    let name = document.getElementById("NamePlaceholder").value;
-    let email = document.getElementById("emailPlaceholder").value;
-    let HelloField = document.getElementsByClassName("formWrapper");
-    HelloField[0].setAttribute(
-      "style",
-      "font-size: 26px; outline: none; margin: 5px;font-family: 'Notable', sans-serif; color: rgb(255, 255, 255); height: 7vh;"
-    );
-    HelloField[0].innerHTML = `Hello ${name}. Ready for more?`;
-    document
-      .querySelector(".header-button")
-      .setAttribute("style", "display:none");
+    if (
+      document.getElementById("NamePlaceholder").value != "" &&
+      document.getElementById("emailPlaceholder").value != ""
+    ) {
+      let name = document.getElementById("NamePlaceholder").value;
+      let email = document.getElementById("emailPlaceholder").value;
+      let HelloField = document.getElementsByClassName("formWrapper");
+      HelloField[0].setAttribute(
+        "style",
+        "font-size: 26px; outline: none; margin: 5px;font-family: 'Notable', sans-serif; color: rgb(255, 255, 255); height: 7vh;"
+      );
+      HelloField[0].innerHTML = `Hello ${name}. Ready for more?`;
+      document
+        .querySelector(".header-button")
+        .setAttribute("style", "display:none");
+    } else {
+      alert("The name field or the email field are empty.");
+    }
   }
   // обработчики кнопок товаров
   else if (event.target.classList.contains("commonMacbtn")) {
@@ -79,12 +49,22 @@ async function clickFun() {
       setTimeout(() => {
         document
           .querySelector(".airMac")
+          .setAttribute("style", "display:flex; opacity: 70%;");
+      }, 40);
+      setTimeout(() => {
+        document
+          .querySelector(".airMac")
           .setAttribute("style", "display:flex; opacity: 100%;");
       }, 70);
     }
     document
       .querySelector(".macBuyForm")
       .setAttribute("style", "display:flex; opacity: 30%;");
+    setTimeout(() => {
+      document
+        .querySelector(".macBuyForm")
+        .setAttribute("style", "display:flex; opacity: 70%;");
+    }, 40);
     setTimeout(() => {
       document
         .querySelector(".macBuyForm")
@@ -132,12 +112,22 @@ async function clickFun() {
     setTimeout(() => {
       document
         .querySelector(".airMacBuyForm")
+        .setAttribute("style", "display:flex; opacity: 70%;");
+    }, 40);
+    setTimeout(() => {
+      document
+        .querySelector(".airMacBuyForm")
         .setAttribute("style", "display:flex; opacity: 100%;");
     }, 70);
     if (document.querySelector(".commonMac").classList.contains("toOpacity")) {
       document
         .querySelector(".commonMac")
         .setAttribute("style", "display:flex; opacity: 30%;");
+      setTimeout(() => {
+        document
+          .querySelector(".commonMac")
+          .setAttribute("style", "display:flex; opacity: 70%;");
+      }, 40);
       setTimeout(() => {
         document
           .querySelector(".commonMac")
@@ -154,11 +144,15 @@ async function clickFun() {
     });
     prom.then(() => {
       document.querySelector(".iseText").innerHTML = `
+      <div class='iphoneContainer'>
       <div class='formInputs'>
       <input type='text' placeholder='First name'  class='buyFormInput'/>
       <input type='text' placeholder='Last name'  class='buyFormInput'/>
       <input type='email' placeholder='email'  class='buyFormInput'/>
-      <input type='submit'  class='buyFormInput'/>`;
+      <input type='submit'  class='buyFormInput'/>
+      </div>
+      <img class='backimg iseBackBtn' src="./img/backimg.png"/>
+      </div>`;
     });
   } else if (event.target.classList.contains("i11Btn")) {
     document
@@ -168,11 +162,15 @@ async function clickFun() {
       .querySelector(".i11Text")
       .setAttribute("style", "transform: translate(-37vw,0);");
     document.querySelector(".i11Text").innerHTML = `
+    <div class='iphoneContainer'>
+    <img class='backimg i11BackBtn' src="./img/backimRev.png"/>
       <div class='formInputs'>
       <input type='text' placeholder='First name'  class='buyFormInput'/>
       <input type='text' placeholder='Last name'  class='buyFormInput'/>
       <input type='email' placeholder='email'  class='buyFormInput'/>
-      <input type='submit'  class='buyFormInput'/>`;
+      <input type='submit'  class='buyFormInput'/>
+      </div>
+      </div>`;
   } else if (event.target.classList.contains("iProBtn")) {
     document
       .querySelector(".iProImg")
@@ -181,13 +179,60 @@ async function clickFun() {
       .querySelector(".iProText")
       .setAttribute("style", "transform: translate(37vw,0);");
     document.querySelector(".iProText").innerHTML = `
+    <div class='iphoneContainer'>
       <div class='formInputs'>
       <input type='text' placeholder='First name'  class='buyFormInput'/>
       <input type='text' placeholder='Last name'  class='buyFormInput'/>
       <input type='email' placeholder='email'  class='buyFormInput'/>
-      <input type='submit'  class='buyFormInput'/>`;
+      <input type='submit'  class='buyFormInput'/>
+      </div>
+      <img class='backimg iProBackBtn' src="./img/backimg.png"/>
+      </div>`;
   }
-
+  //копка назад в поле айфонов
+  else if (event.target.classList.contains("iseBackBtn")) {
+    document
+      .querySelector(".iseImg")
+      .setAttribute("style", "transform: translate(0,0);");
+    document
+      .querySelector(".iseText")
+      .setAttribute("style", "transform: translate(0,0);");
+    document.querySelector(
+      ".iseText"
+    ).innerHTML = `<div class="macnew">New</div>
+  <div class="mactitle">iPhone se</div>
+  <div class="macsubtitle">Lots to love. Less to spend.</div>
+  <div class="price">Starting at $399.</div>
+  <div class="subprice">From $9.54/mo. or $229 with trade-in.</div>
+  <button class="macbtn iseBtn">Buy</button>
+</div>`;
+  } else if (event.target.classList.contains("i11BackBtn")) {
+    document
+      .querySelector(".i11Img")
+      .setAttribute("style", "transform: translate(0,0);");
+    document
+      .querySelector(".i11Text")
+      .setAttribute("style", "transform: translate(0,0);");
+    document.querySelector(".i11Text").innerHTML = `
+    <div class="mactitle">iPhone 11</div>
+          <div class="macsubtitle">Just the right amount of everything.</div>
+          <div class="subprice">From $18.70/mo. or $449 with trade-in.</div>
+          <button class="macbtn i11Btn">Buy</button>`;
+  } else if (event.target.classList.contains("iProBackBtn")) {
+    document
+      .querySelector(".iProImg")
+      .setAttribute("style", "transform: translate(0,0);");
+    document
+      .querySelector(".iProText")
+      .setAttribute("style", "transform: translate(0,0);");
+    document.querySelector(".iProText").innerHTML = `
+    <div class="mactitle">iPhone 11 Pro</div>
+    <div class="macsubtitle">
+      Pro cameras. Pro display. Pro performance.
+    </div>
+    <div class="subprice">From $28.29/mo. or $679 with trade-in.</div>
+    <button class="macbtn iProBtn">Buy</button>`;
+  }
   // обработчик селектора цвета товара мак
   else if (event.target.classList.contains("commonMacSelect")) {
     if (document.querySelector(".commonMacSelect").value == "BLACK") {
